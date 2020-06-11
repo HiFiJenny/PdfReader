ae27ca13f992c15481c693bb12977829bfd72a4f
??? <zhushuojun@163.com>
2020-06-12 06:17:51 +0800

    Initial commit

---
+++ PdfReader/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PdfReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PDFpath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.EXCELpath = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(-2, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择PDF";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PDFpath
+            // 
+            this.PDFpath.BackColor = System.Drawing.Color.Navy;
+            this.PDFpath.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.PDFpath.ForeColor = System.Drawing.SystemColors.Window;
+            this.PDFpath.Location = new System.Drawing.Point(104, 39);
+            this.PDFpath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PDFpath.Name = "PDFpath";
+            this.PDFpath.Size = new System.Drawing.Size(495, 27);
+            this.PDFpath.TabIndex = 1;
+            this.PDFpath.TextChanged += new System.EventHandler(this.PDFpath_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkBlue;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(-2, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "保存地址";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // EXCELpath
+            // 
+            this.EXCELpath.BackColor = System.Drawing.Color.Navy;
+            this.EXCELpath.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.EXCELpath.ForeColor = System.Drawing.SystemColors.Window;
+            this.EXCELpath.Location = new System.Drawing.Point(104, 87);
+            this.EXCELpath.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.EXCELpath.Name = "EXCELpath";
+            this.EXCELpath.Size = new System.Drawing.Size(495, 27);
+            this.EXCELpath.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkBlue;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(-2, 121);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 48);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "转换";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(531, -2);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 30);
+            this.panel1.TabIndex = 6;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "PDF->EXCEL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(609, 167);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.EXCELpath);
+            this.Controls.Add(this.PDFpath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "PDF->EXCEL";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PDFpath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox EXCELpath;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PdfReader/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using org.pdfbox.pdmodel;
+using org.pdfbox.util;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.Runtime.InteropServices;
+using NPOI.SS.UserModel;
+
+namespace PdfReader
+{
+    public partial class Form1 : Form
+    {
+        NPOI.SS.UserModel.ISheet st;
+        pdfexcelpath address = new pdfexcelpath();
+
+        /// <summary>
+        /// 窗体动画函数
+        /// </summary>
+        /// <param name="hwnd">指定产生动画的窗口的句柄</param>
+        /// <param name="dwTime">指定动画持续的时间</param>
+        /// <param name="dwFlags">指定动画类型，可以是一个或多个标志的组合。</param>
+        /// <returns></returns>
+        [DllImport("user32")]
+        private static extern bool AnimateWindow(IntPtr hwnd, int dwTime, int dwFlags);
+
+        //下面是可用的常量，根据不同的动画效果声明自己需要的
+        private const int AW_HOR_POSITIVE = 0x0001;//自左向右显示窗口，该标志可以在滚动动画和滑动动画中使用。使用AW_CENTER标志时忽略该标志
+        private const int AW_HOR_NEGATIVE = 0x0002;//自右向左显示窗口，该标志可以在滚动动画和滑动动画中使用。使用AW_CENTER标志时忽略该标志
+        private const int AW_VER_POSITIVE = 0x0004;//自顶向下显示窗口，该标志可以在滚动动画和滑动动画中使用。使用AW_CENTER标志时忽略该标志
+        private const int AW_VER_NEGATIVE = 0x0008;//自下向上显示窗口，该标志可以在滚动动画和滑动动画中使用。使用AW_CENTER标志时忽略该标志该标志
+        private const int AW_CENTER = 0x0010;//若使用了AW_HIDE标志，则使窗口向内重叠；否则向外扩展
+        private const int AW_HIDE = 0x10000;//隐藏窗口
+        private const int AW_ACTIVE = 0x20000;//激活窗口，在使用了AW_HIDE标志后不要使用这个标志
+        private const int AW_SLIDE = 0x40000;//使用滑动类型动画效果，默认为滚动动画类型，当使用AW_CENTER标志时，这个标志就被忽略
+        private const int AW_BLEND = 0x80000;//使用淡入淡出效果
+        //窗体代码（将窗体的FormBorderStyle属性设置为none）：
+
+        public Form1()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AnimateWindow(this.Handle, 200, AW_BLEND | AW_ACTIVE);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";//注意这里写路径时要用c:\\而不是c:\
+            openFileDialog.Filter = "文本文件|*.*|C#文件|*.cs|所有文件|*.*";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                address.pdf_path = openFileDialog.FileName;
+                if (address.pdf_path.Contains("pdf"))
+                {
+                    PDFpath.Text = address.pdf_path;
+                }
+                else
+                {
+                    MessageBox.Show("文件选择错误");
+                    PDFpath.Text = " ";
+                    address.pdf_path = " ";
+                }
+
+            }
+        }
+
+        public class pdfexcelpath
+        {
+            public string pdf_path;
+            public string excel_path;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog dialogs = new SaveFileDialog();
+            dialogs.Filter = "Excel|*.xlsx";
+            // dialogs.DefaultExt = "*.xlsx";
+            dialogs.FileName = "CalCer.xlsx";
+            if (dialogs.ShowDialog() == DialogResult.OK)
+            {
+                address.excel_path = dialogs.FileName;
+                EXCELpath.Text = address.excel_path;
+            }
+        }
+
+            private void button3_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            FileStream fs;
+            XSSFWorkbook wk;
+            PDDocument doc = PDDocument.load(address.pdf_path);
+            PDFTextStripper pdfstripper = new PDFTextStripper();
+            string str = pdfstripper.getText(doc);
+            string[] str1 = str.Split(new string[] { "Sensor" }, StringSplitOptions.RemoveEmptyEntries);       //提取sensor之后的内容：数据
+            string[] str2 = str1[1].Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);  //提取每一行的数据
+            string[] standard = str2[0].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries); //提取第一行标准值   
+            //   swPdfChange.Write(str1[1]);  //str1 中保存所需要的内容： 标准测量点 校准值
+            int num = str2.Length - 1;   //获取的值的行数
+                                         //    swPdfChange.Close();
+            string TempletFileName =  @".\excel.xlsx";  //模板文件  
+
+            using ( fs = File.OpenRead(TempletFileName))
+            {
+                FileStream fs2 = File.Create(address.excel_path);
+                wk = new XSSFWorkbook(fs);
+
+                ICellStyle tableStyle = wk.CreateCellStyle();
+                st = wk.GetSheet("52-301,52-301.1");
+                IRow r = st.CreateRow(0);
+                ICell c = null;
+                int cnt = 0;
+                for (int i = 0; i < num; i++)
+                {
+                    string[] value = str2[i + 1].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    if ((i % 4 ==0) && (i >= 4 ))
+                    {
+                        cnt += 24;
+                        goto A;
+                    }
+                A:
+
+                    for (int j = 0; j < standard.Length; j++)        //标准值/测量点写入excel
+                    {
+                            char[] TrimChar = { '(', ')' };
+                            int[] col = { 1, 2, 14 };
+                            string[] val = { value[0], standard[j], value[(j + 1) * 2].Trim(TrimChar) };
+
+                            r = st.CreateRow(i * 5 + 68 + j + cnt);
+                            for (int dic = 0; dic < 3; dic++)
+                            {
+                                c = r.CreateCell(col[dic]);
+                                c.SetCellValue(val[dic]);
+                        }
+
+                            for (int k = 0; k < 8; k++)        //4次读数
+                            {
+                                c = r.CreateCell(k + 5);
+                                c.SetCellValue(value[(j + 1) * 2 - 1]);
+                            }
+
+                    }
+                    c.CellStyle.BorderBottom = NPOI.SS.UserModel.BorderStyle.Thin;
+                    c.CellStyle.BorderLeft = NPOI.SS.UserModel.BorderStyle.Thin;
+                    c.CellStyle.BorderRight = NPOI.SS.UserModel.BorderStyle.Thin;
+                }
+
+                wk.Write(fs2);
+                fs2.Close();
+                fs.Close();
+            }
+            this.Cursor = Cursors.Arrow;
+            MessageBox.Show("转换完成", "PDF->Excel", MessageBoxButtons.OK);
+        }
+
+
+        /// <summary>
+        /// 顶部控件： 关闭窗口 拖动窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private Point mousePoint = new Point();
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            this.mousePoint.X = e.X;
+            this.mousePoint.Y = e.Y;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Top = Control.MousePosition.Y - mousePoint.Y;
+                this.Left = Control.MousePosition.X - mousePoint.X;
+            }
+        }
+
+
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            AnimateWindow(this.Handle, 200, AW_BLEND | AW_HIDE);
+        }
+
+        private void PDFpath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
